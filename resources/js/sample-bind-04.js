df535ae85267e3f68ee1c1f0856628399f2222ab
Thiago Senna <trsenna@gmail.com>
2021-01-30 19:12:43 -0300

    samples underscores bind ok

---
+++ resources/js/sample-bind-04.js
@@ -0,0 +1,21 @@
+( function () {
+
+    const Hello = function () {
+
+        this.goodMorning = function () {
+
+            console.log( 'Sample #4: Hello, good morning!' );
+            console.log( this ); // Object { goodMorning: ... }
+
+        };
+
+    };
+
+    const hello = new Hello();
+
+    hello.goodMorning = _.bind( hello.goodMorning, hello );
+    
+
+    setTimeout( hello.goodMorning );
+
+} )();
