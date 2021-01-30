9d5bedc4bce1771d5cb59851d1e9168f9d29c680
Thiago Senna <trsenna@gmail.com>
2021-01-30 19:25:21 -0300

    _.bindAll sample added

---
+++ resources/js/sample-bind-05.js
@@ -0,0 +1,21 @@
+( function () {
+
+    const Hello = function () {
+
+        this.goodMorning = function () {
+
+            console.log( 'Sample #5: Hello, good morning!' );
+            console.log( this ); // Object { goodMorning: ... }
+
+        };
+
+    };
+
+    const hello = new Hello();
+
+    _.bindAll( hello, 'goodMorning' );
+    
+
+    setTimeout( hello.goodMorning );
+
+} )();
