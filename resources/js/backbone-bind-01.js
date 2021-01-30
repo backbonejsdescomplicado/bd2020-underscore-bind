595aa98df441e4fc80e53c696cbd7e0ae14271ea
Thiago Senna <trsenna@gmail.com>
2021-01-30 19:42:59 -0300

    backbone.model _.bindAll sample

---
+++ resources/js/backbone-bind-01.js
@@ -0,0 +1,24 @@
+( function () {
+
+    const Hello = Backbone.Model.extend( {
+
+        initialize: function() {
+
+            _.bindAll(this, 'goodMorning');
+
+        },
+
+        goodMorning: function() {
+
+            console.log( 'Backbone.Model Sample #1: Hello, good morning!' );
+            console.log( this ); // Object { goodMorning: ... }
+
+        },
+
+    } );
+
+    const hello = new Hello();
+
+    setTimeout( hello.goodMorning );
+
+} )();
